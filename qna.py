7e54ce6391e617debbcbcccfa4808cbb11c7f3d7
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-05-17 22:50:15 +0800

    Implement QnA fine-tune

---
+++ qna.py
@@ -0,0 +1,50 @@
+import time
+from dotenv import load_dotenv
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain import OpenAI
+
+def process_data():
+    print("Getting data...")
+    loader = DirectoryLoader("documents", glob="**/*.txt")
+    documents = loader.load()
+    print("Documents loaded.")
+    
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    print("Text splitted.")
+    
+    texts = text_splitter.split_documents(documents)
+    
+    print(texts)
+    
+    return texts
+
+def get_embeddings(texts):
+    print("Getting embeddings...")
+    embeddings = OpenAIEmbeddings()
+    print("Embeddings loaded.")
+    
+    print("Creating vector store...")
+    vector_store = Chroma.from_documents(texts, embeddings)
+    print("Vector store created.")
+    
+    qna = RetrievalQA.from_chain_type(
+        llm=OpenAI(),
+        chain_type="stuff",
+        retriever=vector_store.as_retriever()
+    )
+    
+    return qna
+
+def query(prompt, qna_retriever):
+    print(f"User prompt: {prompt}")
+    print(f"Answer: {qna_retriever.run(prompt)}")
+
+if __name__ == '__main__':
+    load_dotenv()
+    texts = process_data()
+    qna_retriever = get_embeddings(texts)
+    query("What did Ichikawa Danjuro star in?", qna_retriever)
